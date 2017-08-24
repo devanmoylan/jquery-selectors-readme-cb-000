6e6feacf643440707b2edab22c28d0d8ed8f846a
Devan <devan.moylan@backyardlion.com>
2017-08-24 17:47:17 -0600

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -5,3 +5,7 @@ function paragraphSelector() {
 function lastImageSelector() {
     return $('img:last')
 }
+
+function ninjaBabySelector() {
+    return $("img[src='https://s3.amazonaws.com/after-school-assets/baby-ninja.gif']")
+}
